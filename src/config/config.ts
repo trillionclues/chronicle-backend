ca59ab5a3c66baa71e75b64ed93fc0a7a1d6cb8a
trillionclues <trillionclues@gmail.com>
2025-04-06 08:22:23 +0100

    Remove dist directory from version control

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,14 +6,15 @@ interface Config {
   port: number;
   nodeEnv: string;
   MONGO_URI: string;
-  FIREBASE_SERVICE_ACCOUNT: string;
+  FIREBASE_SERVICE_ACCOUNT_PATH: any;
 }
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
   nodeEnv: process.env.NODE_ENV || "development",
   MONGO_URI: process.env.MONGO_URI || "",
-  FIREBASE_SERVICE_ACCOUNT: process.env.FIREBASE_SERVICE_ACCOUNT || "",
+  FIREBASE_SERVICE_ACCOUNT_PATH:
+    process.env.FIREBASE_SERVICE_ACCOUNT_PATH || "",
 };
 
 export default config;
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,11 +1,41 @@
 import admin from "firebase-admin";
-// const serviceAccount = require("../chronicle-86dce-firebase-adminsdk-fbsvc-1155616c1c.json");
+import fs from "fs";
+import path from "path";
+// import serviceAccount from "../chronicle-86dce-firebase-adminsdk-fbsvc-1155616c1c.json";
 import config from "./config";
 
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
+  try {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+  } catch (error) {
+    console.error("Error parsing FIREBASE_SERVICE_ACCOUNT_JSON:", error);
+    process.exit(1);
+  }
+} else if (config.FIREBASE_SERVICE_ACCOUNT_PATH) {
+  try {
+    const filePath = path.resolve(config.FIREBASE_SERVICE_ACCOUNT_PATH);
+    if (fs.existsSync(filePath)) {
+      serviceAccount = JSON.parse(fs.readFileSync(filePath, "utf8"));
+    } else {
+      console.error(`Service account file not found at: ${filePath}`);
+      process.exit(1);
+    }
+  } catch (error) {
+    console.error("Error loading service account file:", error);
+    process.exit(1);
+  }
+} else {
+  console.error("No Firebase service account configuration found");
+  process.exit(1);
+}
+
+console.log("config", serviceAccount);
+// console.log(serviceAccount);
+
 admin.initializeApp({
-  credential: admin.credential.cert(
-    config.FIREBASE_SERVICE_ACCOUNT as admin.ServiceAccount
-  ),
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
 
 export default admin;
