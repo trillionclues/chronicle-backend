7c0f343ce8359471130a1475e7d6611561e6480d
trillionclues <trillionclues@gmail.com>
2025-04-06 07:39:57 +0100

    chronicle server completed

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,11 +1,9 @@
 import admin from "firebase-admin";
-// const serviceAccount = require("../chronicle-86dce-firebase-adminsdk-fbsvc-1155616c1c.json");
+const serviceAccount = require("../chronicle-86dce-firebase-adminsdk-fbsvc-1155616c1c.json");
 import config from "./config";
 
 admin.initializeApp({
-  credential: admin.credential.cert(
-    config.FIREBASE_SERVICE_ACCOUNT as admin.ServiceAccount
-  ),
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
 
 export default admin;
